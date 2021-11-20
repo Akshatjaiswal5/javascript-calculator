d6ba1d8835e4dcef3dab9eb0a3a5bb45155597b0
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2021-11-20 17:41:44 +0530

    initial files

---
+++ main.js
@@ -0,0 +1,10 @@
+const keys=document.querySelectorAll('.key');
+keys.forEach(key=>key.addEventListener('click',event=>handleKeyPress(event.target)));
+
+const display=document.getElementById('display');
+
+
+
+function handleKeyPress(key){
+
+}
